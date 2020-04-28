999652bd0f859a64f581b96b569e2a74c328b134
Harshada-github <harshadalondhal97@gmail.com>
2020-04-28 22:17:42 +0530

    all programs

---
+++ programs/count.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int nl=0,nb=0,nt=0,ch;
+
+	while((ch=getchar()) != EOF)
+	{
+		if(ch=='\n')
+		  nl++;
+
+		if(ch==' ')
+		  nb++;
+
+		if(ch=='\t')
+		  nt++;
+	}
+	printf("new lines:%d \n Tabs:%d \n Blank space:%d",nl,nt,nb);
+	return 0;
+}
+
+

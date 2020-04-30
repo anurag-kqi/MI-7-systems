da41ef881e967c90229a6367919f04c28fad4689
Harshada-github <64095932+Harshada-github@users.noreply.github.com>
2020-04-30 16:04:32 +0530

    Update ascii.c

--- Harsha/ascii.c
+++ Harsha/ascii.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-int main() 
-{
-  char a;
-  char b = 'p';
-  printf("Enter any charter:");
-  scanf("%c", &a );
+#include <stdio.h>
 
-  printf("ASCII value of %c is %d\n", a, a);
-  printf("ASCII value of p is %d\n", b );
-  return 0;
+int 
+main() 
+{
+    char a;
+    char b = 'p';
+    printf("Enter any charter:");
+    scanf("%c", &a);
+
+    printf("ASCII value of %c is %d\n", a, a);
+    printf("ASCII value of p is %d\n", b );
+    return 0;
 }

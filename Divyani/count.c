5c3389c6b08bb61819232de0c19ad5c2abc6e7e9
Divyani286 <divyanipatidar307@gmail.com>
2020-05-04 16:16:59 +0530

    My first update program

--- Divyani/count.c
+++ Divyani/count.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
-
-int main() {
-
-    int char_count, num_lines, num_tabs, num_spaces;
-
+int 
+main() 
+{
+   int char_count, num_lines, num_tabs, num_spaces;
 
     num_lines = 0;
     num_tabs = 0;
     num_spaces = 0;
 
     while ((char_count = getchar()) != EOF) {
-        if (char_count == '\n')
-            ++num_lines;
-        if (char_count == '\t')
-            ++num_tabs;
-        if (char_count == ' ')
-            ++num_spaces;
+        if (char_count == '\n'){
+            ++num_lines;}
+        if (char_count == '\t'){
+            ++num_tabs;}
+        if (char_count == ' '){
+            ++num_spaces;}
     }
-
     printf("Blanks: %d\nTabs: %d\nNewlines: %d\n", num_spaces, num_tabs, num_lines);
-
 }
 

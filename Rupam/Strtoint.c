16a37e2a883a6ef63834862d4278dea36e6ac9c5
rupam88 <rupsdan@gmail.com>
2020-05-05 23:32:55 +0530

    Convert String To Int

---
+++ Rupam/Strtoint.c
@@ -0,0 +1,18 @@
+//Program for converting string to integer
+
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+ {
+   char str[30]="22welcome";
+
+   int num=atoi(str);
+
+   printf("converting 22welcome: %d\n", num);
+
+   return 0;
+ }
+ 
+
+

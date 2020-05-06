8a6412bca68522bd1dfe905d096b24bdc23036b1
rupam88 <56496550+rupam88@users.noreply.github.com>
2020-05-07 01:19:59 +0530

    String to int

--- Rupam/Strtoint.c
+++ Rupam/Strtoint.c
@@ -3,16 +3,14 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int main()
- {
-   char str[30]="22welcome";
-
-   int num=atoi(str);
-
-   printf("converting 22welcome: %d\n", num);
-
+int
+main()
+    {
+   char str[30] = "22welcome";
+   int num = atoi(str);
+   printf(" converting 22welcome: %d\n ", num);
    return 0;
- }
+   }
  
 
 

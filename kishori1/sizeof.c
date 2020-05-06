7756c8bd00e85db3fb56fdbaffa52bbb8658510b
kishori543 <kishoripnikam543@gmail.com>
2020-05-06 21:38:20 +0530

    data types

---
+++ kishori1/sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int intType;
+    float floattType;
+    char charType;
+    double doubleType;
+    printf("size of int: %ld bytes\n", sizeof (intType));
+    printf("size of float: %ld bytes\n", sizeof (floattType));
+    printf("size of char: %ld bytes\n", sizeof (charType));
+    printf("size of double: %ld bytes\n", sizeof(doubleType));
+
+    return 0;
+}

72143cdf6eeab299f7c4de9303a5c98075c6a409
kishori543 <kishoripnikam543@gmail.com>
2020-05-08 18:40:20 +0530

    updated pgm

--- kishori1/if.c
+++ kishori1/if.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
 int main()
 {
-    int num;
-
-    printf("enter a number: \n");
-    scanf("%d", &num);
+  int num;
+  printf("enter a number:\n");
+  scanf("%d",&num);
+  if (num >= 0)
+  {
     if (num > 0)
-        printf("%d is a positive number \n", num);
-    else if (num < 0)
-        printf("%d is a negative number \n",num);
+      printf("%d is a positive number \n",num);
     else
-        printf("0 is neither positive nor negative");
+      printf("you have entered value zero");
+  }
+  else
+  printf("%d is negative num",num);
+  return 0;
 }
--- kishori1/sizeof.c
+++ kishori1/sizeof.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int intType;
-    float floattType;
-    char charType;
-    double doubleType;
-    printf("size of int: %ld bytes\n", sizeof (intType));
-    printf("size of float: %ld bytes\n", sizeof (floattType));
-    printf("size of char: %ld bytes\n", sizeof (charType));
-    printf("size of double: %ld bytes\n", sizeof(doubleType));
-
-    return 0;
+  int intType;
+  float floattType;
+  char charType;
+  double doubleType;
+  printf("size of int: %ld bytes\n", sizeof (intType));
+  printf("size of float: %ld bytes\n", sizeof (floattType));
+  printf("size of char: %ld bytes\n", sizeof (charType));
+  printf("size of double: %ld bytes\n", sizeof(doubleType));
+  return 0;
 }

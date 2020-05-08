f8d5c7ba18a75e2cfb5265f9388121110a7c15ac
anurag-kqi <nayan.shrivastava20@gmail.com>
2020-05-08 21:05:07 +0530

    array.c

---
+++ Nayan_S/array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a[]={10, 43, 22, 58, 98, 212, 25, 74, 143, 999};
+
+    for(int i = 0 ; i <= 9 ; i++)
+        {
+            printf("Entered Numbers are %d\n", a[i]);	
+        }
+}

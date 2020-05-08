7429fffe235ea9d13404a12953c77c6a1c16ef46
anurag-kqi <nayan.shrivastava20@gmail.com>
2020-05-08 20:49:40 +0530

    check.c

---
+++ Nayan_S/check.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#inlcide <conio.h>
+
+void main()
+{
+   int n;   
+
+   printf("Enter the Number");
+   scanf("%d", n);
+
+   if(n>=o)
+     {
+         printf("Number is Positive");
+     }
+   else
+     {
+         printf("Number is Negative");
+     }
+}

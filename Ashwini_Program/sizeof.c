684804c87c6184594bb54a8fa1c4cc6d96c55034
ASHWINI1494 <ashwinigarje1494@gmail.com>
2020-05-10 19:13:05 +0530

    size of datatypes

---
+++ Ashwini_Program/sizeof.c
@@ -0,0 +1,17 @@
+/* program to find size of data types*/
+
+#include <stdio.h>
+
+int
+main()
+{
+  int int_type;
+  float float_type;
+  char char_type;
+  double double_type;
+  printf("size of int: %ld bytes\n", sizeof (int_type));
+  printf("size of float: %ld bytes\n", sizeof (float_tpye));
+  printf("size of char: %ld bytes\n", sizeof (char_tpye));
+  printf("size of double: %ld bytes\n", sizeof(double_type));
+  return 0;
+}

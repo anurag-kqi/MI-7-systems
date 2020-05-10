fdedad73962170ec508af27d8d43b7717e3abddf
ASHWINI1494 <ashwinigarje1494@gmail.com>
2020-05-10 20:15:39 +0530

    update sizeof.c

--- Ashwini_Program/sizeof.c
+++ Ashwini_Program/sizeof.c
@@ -5,13 +5,13 @@
 int
 main()
 {
-  int inttype;
-  float floattype;
-  char chartype;
-  double doubletype;
-  printf("size of int: %ld bytes\n", sizeof (inttype));
-  printf("size of float: %ld bytes\n", sizeof (floattpye));
-  printf("size of char: %ld bytes\n", sizeof (chartpye));
-  printf("size of double: %ld bytes\n", sizeof(doubletype));
+  int intType;
+  float floatType;
+  char charType;
+  double doubleType;
+  printf("size of int: %ld bytes\n", sizeof (intType));
+  printf("size of float: %ld bytes\n", sizeof (floatTpye));
+  printf("size of char: %ld bytes\n", sizeof (charTpye));
+  printf("size of double: %ld bytes\n", sizeof(doubleType));
   return 0;
 }

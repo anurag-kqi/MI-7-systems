5b954eec68da3cb49f135375a1fedbe85c45f01e
ASHWINI1494 <ashwinigarje1494@gmail.com>
2020-05-10 20:56:47 +0530

    update sizeof.c

--- Ashwini_Program/sizeof.c
+++ Ashwini_Program/sizeof.c
@@ -1,15 +1,16 @@
 /* program to find size of data types*/
 
 #include <stdio.h>
-int main()
+int 
+main()
 {
   int intType;
   float floatType;
   char charType;
   double doubleType;
-  printf("size of int: %ld bytes\n", sizeof (intType));
-  printf("size of float: %ld bytes\n", sizeof (floatTpye));
-  printf("size of char: %ld bytes\n", sizeof (charTpye));
-  printf("size of double: %ld bytes\n", sizeof(doubleType));
+  printf("size of int: %d bytes\n", sizeof (intType));
+  printf("size of float: %f bytes\n", sizeof (floatType));
+  printf("size of char: %c bytes\n", sizeof (charType));
+  printf("size of double: %d bytes\n", sizeof(doubleType));
   return 0;
 }

1598768c80efa1acbfb4cc2164b320e9b1c041f5
anurag-kqi <nayan.shrivastava20@gmail.com>
2020-05-13 15:32:36 +0530

    Stucture to pointer

---
+++ Nayan_S/structure2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+struct student
+{
+    char name[20];
+    int rollno;
+}; 
+
+void
+main()
+{
+    struct student s1;  //ordinary type of student variable.
+    struct student *p;  // pointer type of student variable.
+
+    p = &s1; //storing address of s1 in pointer p variable.
+  
+    printf("Enter the student name\n");
+    scanf("%s\n", p->name);
+    printf("Enter the Roll No. of student");
+    scanf("%d\n", &p->rollno);
+    printf("-------------------------------------");
+    printf("The Name of student is: %s", (*p).name);
+    printf("The Roll No. of student is: %d", (*p).rollno);
+}

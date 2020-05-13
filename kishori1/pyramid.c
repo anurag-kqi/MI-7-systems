d5d5d509628b000d49ceb4054dab2c7e70d4bae0
kishori543 <kishoripnikam543@gmail.com>
2020-05-13 18:51:07 +0530

    struct to pointer

--- kishori1/pyramid.c
+++ kishori1/pyramid.c
@@ -9,7 +9,7 @@ main()
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = 1; i <= rows; i++) {
-      for (j = 1; j <= i; j++) {
+      for (j = 1; j = i + 1; j++) {
          printf("* ");
       }
       printf("\n");

e1360c4261a2abf26a65f02527270678e21de665
ASHWINI1494 <ashwinigarje1494@gmail.com>
2020-05-13 23:03:39 +0530

    simple structures program

---
+++ sizeof.c
@@ -0,0 +1,17 @@
+/* program to find size of data types*/
+
+#include <stdio.h>
+
+int
+main()
+{
+  int inttype;
+  float floattype;
+  char chartype;
+  double doubletype;
+  printf("size of int: %ld bytes\n", sizeof (inttype));
+  printf("size of float: %ld bytes\n", sizeof (floattpye));
+  printf("size of char: %ld bytes\n", sizeof (chartpye));
+  printf("size of double: %ld bytes\n", sizeof(doubletype));
+  return 0;
+}

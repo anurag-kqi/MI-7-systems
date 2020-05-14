2c48c865f38e64c6be338e57c0360e140be64044
sakshi142 <64999586+sakshi142@users.noreply.github.com>
2020-05-14 21:06:45 +0530

    Update struct3.c

--- Sakshi/struct3.c
+++ Sakshi/struct3.c
@@ -10,37 +10,39 @@ struct Student
 
 void PassBy_Value(struct Student Students);
 
-int main()
+int
+main()  
 {
   struct Student Student1;
 
-  printf("\nPlease Enter the Student Name \n");
-  scanf("%s",&Student1.Student_Name);
+  printf("Please Enter the Student Name \n");
+  scanf("%s", &Student1.Student_Name);
   
-  printf("\nPlease Enter Student Inter First Year Marks \n");
-  scanf("%f",&Student1.First_Year_Marks);
+  printf("Please Enter Student Inter First Year Marks \n");
+  scanf("%f", &Student1.First_Year_Marks);
   
-  printf("\nPlease Enter Student Inter Second Year Marks \n");
-  scanf("%f",&Student1.Second_Year_Marks);
+  printf("Please Enter Student Inter Second Year Marks \n");
+  scanf("%f", &Student1.Second_Year_Marks);
   
   PassBy_Value(Student1);   
   return 0;
 }
 
 void PassBy_Value(struct Student Students)
+  
 {
   float Sum, Average;
 
   Sum = Students.First_Year_Marks + Students.Second_Year_Marks;
   Average = Sum/2;
   
-  if(Average > 900)
-  {
-    printf("\n %s is Eligible for Scholorship",Students.Student_Name); 
-  }
-  else
-  {
-    printf("\n %s is Not Eligible for Scholorship",Students.Student_Name); 
-  }
+    if(Average > 900)
+        {
+           printf(" %s is Eligible for Scholorship", Students.Student_Name); 
+        }
+       else
+        {
+           printf(" %s is Not Eligible for Scholorship", Students.Student_Name); 
+        }
 
 }

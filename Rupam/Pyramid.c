af804f83252b32201e1ee3297c89aa4b905f8e1b
rupam88 <56496550+rupam88@users.noreply.github.com>
2020-05-14 19:39:08 +0530

    Half Pyramid Program

--- Rupam/Pyramid.c
+++ Rupam/Pyramid.c
@@ -1,4 +1,5 @@
 //Print Half Pyramid of *
+
 #include <stdio.h>
 
 int 
@@ -6,13 +7,13 @@ main()
     {
     int i, j, rows;
 
-    printf("Enter the number of rows");
+    printf(" Enter the number of rows\n ");
     scanf("%d", &rows);
 
     for(i = 1; i <= rows; ++i)
     {
        for(j = 1; j <= i; ++j)
-    {
+      {
     printf("* ");
     }
     printf("\n");

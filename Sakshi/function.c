e18313f030c5efa30cfb91221ab5b18cc365443c
sakshi142 <64999586+sakshi142@users.noreply.github.com>
2020-05-15 07:53:01 +0530

    Update function.c

--- Sakshi/function.c
+++ Sakshi/function.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-void swap(int a, int b);          
- 
+
+void swap(int a, int b); 
+
 int 
 main()
 {
-    int m = 10, n = 20;
-    // calling swap function by value
+    int m = 10, n = 2;
+ 
     printf(" values before swap  m = %d \nand n = %d", m, n);
     swap(m, n);                         
 }

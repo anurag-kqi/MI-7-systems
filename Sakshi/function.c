b355ab8310771bac43c04fbc2d3d509a8b1767ee
sakshi142 <sakshibelpande29@gmail.com>
2020-05-15 07:44:22 +0530

    function.c

---
+++ Sakshi/function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int a, int b);          
+ 
+int 
+main()
+{
+    int m = 10, n = 20;
+    // calling swap function by value
+    printf(" values before swap  m = %d \nand n = %d", m, n);
+    swap(m, n);                         
+}
+ 
+void swap(int a, int b)
+{ 
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+    printf(" \nvalues after swap m = %d\n and n = %d", a, b);
+}

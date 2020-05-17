8a51854d44b42ee18058559249d0c800b7e7287c
Ashwini505 <ashparab1234@gmail.com>
2020-05-17 20:23:30 +0530

    My updated prgm

--- AshwiniP/logical.c
+++ AshwiniP/logical.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-
 void
-main(){
+main()
+{
+	int a, b;
+	int sum, sub, div, mult;
 
-int a,b;
-int sum,sub,div,mult;
-printf("Enter number:");
-scanf("%d %d",&a,&b);
-sum=a+b;
-sub=a-b;
-div=a/b;
-mult=a*b;
-printf("\nAddition=%d", sum);
-printf("\nSubstraction=%d", sub);
-printf("\nDivision=%d", div)
-printf("\nMultiplication=%d", mult);
+	printf("Enter number:");
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	sub = a - b;
+	div = a / b;
+	mult = a * b;
+	printf("\nAddition=%d", sum);
+	printf("\nSubstraction=%d", sub);
+	printf("\nDivision=%d", div)
+	printf("\nMultiplication=%d", mult);
 } 

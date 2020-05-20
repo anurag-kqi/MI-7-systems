f098b95e0818dd1a8da36d29dba0643e37fe5002
rupam88 <56496550+rupam88@users.noreply.github.com>
2020-05-20 23:33:14 +0530

    Update LinkedReverse.c

--- Rupam/LinkedReverse.c
+++ Rupam/LinkedReverse.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 struct node{
-    int num;                    //Data of the node
-    struct node *nextptr;       //Address of the node
+   int num;                    //Data of the node
+   struct node *nextptr;       //Address of the node
   }*stnode;
 
 void createNodeList(int n);     //function to create the list
@@ -15,61 +15,61 @@ void displayList();             //function to display the list
 int 
 main()
   {
-    int n;
+   int n;
 		
-    printf("\n\n Linked List : Create a singly linked list and print it in reverse order :\n");
-    printf("******************************************************************************\n");
+   printf("\n\n Linked List : Create a singly linked list and print it in reverse order :\n");
+   printf("******************************************************************************\n");
 
-    printf(" Input the number of nodes : ");
-    scanf("%d", &n);
+   printf(" Input the number of nodes : ");
+   scanf("%d", &n);
 	
-    createNodeList(n);
-    printf("\n Data entered in the list are : \n");
+   createNodeList(n);
+   printf("\n Data entered in the list are : \n");
     
-    displayList();
-    reverseDispList();
-    printf("\n The list in reverse are :  \n");
-    displayList();
-    return 0;
+   displayList();
+   reverseDispList();
+   printf("\n The list in reverse are :  \n");
+   displayList();
+   return 0;
   }
 
-void createNodeList(int n)
+void 
+createNodeList(int n)
   {
-    struct node *fnNode, *tmp;
-    int num, i;
+   struct node *fnNode, *tmp;
+   int num, i;
   
-    stnode = (struct node *)malloc(sizeof(struct node));
+   stnode = (struct node *)malloc(sizeof(struct node));
     
-    if(stnode == NULL) //check whether the stnode is NULL and if so no memory allocation
-       {
-         printf(" Memory can not be allocated.");
-       }
-     else{
-         printf(" Input data for node 1 : ");    // reads data for the node through keyboard
-	 scanf("%d", &num);
+   if(stnode == NULL) //check whether the stnode is NULL and if so no memory allocation
+   {
+     printf(" Memory can not be allocated.");
+   }
+   else{
+     printf(" Input data for node 1 : ");    // reads data for the node through keyboard
+     scanf("%d", &num);
+ 
+     stnode-> num = num;
+     stnode-> nextptr = NULL; //Links the address field to NULL
+     tmp = stnode;
 
-	 stnode-> num = num;
-         stnode-> nextptr = NULL; //Links the address field to NULL
-	 tmp = stnode;
-
-     for(i=2; i<=n; i++)                        // Creates n nodes and adds to linked list
-      {
-	 fnNode = (struct node *)malloc(sizeof(struct node));
-	 if(fnNode == NULL) //check whether the fnnode is NULL and if so no memory allocation
-	   {
-	     printf(" Memory can not be allocated.");
-		break;
-	     }
-        else
-	     {
-              printf(" Input data for node %d : ", i);
-	      scanf(" %d", &num);
+    for(i=2; i<=n; i++)                        // Creates n nodes and adds to linked list
+     {
+	fnNode = (struct node *)malloc(sizeof(struct node));
+        if(fnNode == NULL) //check whether the fnnode is NULL and if so no memory allocation
+	  {
+	    printf(" Memory can not be allocated.");
+            break;
+	  }
+      else{
+            printf(" Input data for node %d : ", i);
+	    scanf(" %d", &num);
 		
-	      fnNode->num = num;      // links the num field of fnNode with num
-	      fnNode->nextptr = NULL; // links the address field of fnNode with NULL
-	      tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
-	      tmp = tmp->nextptr;
-	    }
+	    fnNode->num = num;      // links the num field of fnNode with num
+	    fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+	    tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+	    tmp = tmp->nextptr;
+	  }
 	}
     }
 }
@@ -77,9 +77,9 @@ void createNodeList(int n)
 void 
 reverseDispList()
   {
-    struct node *prevNode, *curNode;
+   struct node *prevNode, *curNode;
 
-    if(stnode != NULL)
+   if(stnode != NULL)
     {
 	prevNode = stnode;
 	curNode = stnode->nextptr;
@@ -88,32 +88,32 @@ reverseDispList()
 	prevNode->nextptr = NULL; //convert the first node as last
 
 	while(stnode != NULL)
-	{
+         {
 	    stnode = stnode->nextptr;
 	    curNode->nextptr = prevNode;
 
 	    prevNode = curNode;
 	    curNode = stnode;
-	}
+	 }
 	stnode = prevNode; //convert the last node as head
     }
 }
 
 void
 displayList()
+   {
+    struct node *tmp;
+    if(stnode == NULL)
     {
-     struct node *tmp;
-     if(stnode == NULL)
-     {
-	printf(" No data found in the list.");
-     }
-     else{
+       printf(" No data found in the list.");
+    }
+    else{
 	tmp = stnode;
 	while(tmp != NULL)
-	{
+	 {
 	    printf(" Data = %d\n", tmp->num);   // prints the data of current node
 	    tmp = tmp->nextptr;                 // advances the position of current node
 	}
-   }
+    }
 }
 

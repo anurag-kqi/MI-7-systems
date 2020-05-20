a993993b0f046ea933c02257b8e132c064ae1718
sakshi142 <sakshibelpande29@gmail.com>
2020-05-20 23:00:31 +0530

    linklist

--- Sakshi/linklist.c
+++ Sakshi/linklist.c
@@ -1,4 +1,3 @@
-Live Demo
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +9,6 @@ struct node {
 struct node *head = NULL;
 struct node *current = NULL;
 
-//display the list
 void printList() {
 
    struct node *ptr = head;
@@ -26,7 +24,8 @@ void printList() {
 }
 
 //insert link at the first location
-void insert(int data) {
+void insert(int data) 
+{
    
    struct node *link = (struct node*) malloc(sizeof(struct node));
 

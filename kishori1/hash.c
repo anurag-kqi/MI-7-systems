6aa714b609e2f3c188d2d38db0460cb92b2f5f4a
kishori543 <kishoripnikam543@gmail.com>
2020-05-21 18:18:03 +0530

    my hashtable

--- kishori1/hash.c
+++ kishori1/hash.c
@@ -1,7 +1,7 @@
-/* c program to insert a value in hash table*/
+/* c program to insert, delet and search a value in hash table*/
 
 #include<stdio.h>
-#define size 7
+#define size 10
 
 int arr[size];
 
@@ -21,8 +21,26 @@ void insert(int value)
     }
     else {
       printf("Collision : arr[%d] has element %d already!\n",key,arr[key]);
-      printf("Unable to insert %d\n",value);
+        printf("Unable to insert %d\n",value);
     }
+}
+
+void del(int value)
+{
+    int key = value % size;
+    if(arr[key] == value)
+        arr[key] = -1;
+    else
+        printf("%d not present in the hash table\n",value);
+}
+
+void search(int value)
+{
+    int key = value % size;
+    if(arr[key] == value)
+        printf("Search Found\n");
+    else
+        printf("Search Not Found\n");
 }
 
 void print()
@@ -37,12 +55,30 @@ main()
 {
     init();
     /*key = element % size of array*/
-    insert(10);
-    insert(4);
-    insert(2);
+    insert(12);
+    insert(8);
+    insert(13);
     insert(3);
 
     printf("Hash table\n");
     print();
     printf("\n");
+
+    printf("Deleting value 12..\n");
+    del(12);
+    printf("After the deletion hash table\n");
+    print();
+    printf("\n");
+
+    printf("Deleting value 5..\n");
+    del(5);
+    printf("After the deletion hash table\n");
+    print();
+    printf("\n");
+
+    printf("Searching value 8..\n");
+    search(8);
+    printf("Searching value 12..\n");
+    search(12);
+    return 0;
 }

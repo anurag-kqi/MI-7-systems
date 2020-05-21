9e083143a2668ae542ca44ea4646a7fd72de4d00
rupam88 <56496550+rupam88@users.noreply.github.com>
2020-05-21 23:57:20 +0530

    Hash Table Program

--- Rupam/HashTable.c
+++ Rupam/HashTable.c
@@ -1,22 +1,22 @@
 //Hash Table Program using array
 
-#include<stdio.h>
+#include <stdio.h>
 #define size 7
 
 int arr[size];
 
 void
 init()
- {
+{
    int i;
    for(i = 0; i < size; i++)
    arr[i] = -1;
    printf("Values%d",arr[i]);
- }
+}
 
 void
 insert(int value)
- {
+{
     int key = value % size;
 
     if(arr[key] == -1)
@@ -27,41 +27,41 @@ insert(int value)
 	  printf("Collision : arr[%d] has element %d already!\n",key,arr[key]);
 	  printf("Unable to insert %d\n",value);
      }
- }
+}
 
 void
 del(int value)
- {
+{
     int key = value % size;
 
     if(arr[key] == value)
        arr[key] = -1;
      else
        printf("%d not present in the hash table\n",value);
- }
+}
 
 void
 search(int value)
- {
+{
     int key = value % size;
 
     if(arr[key] == value)
        printf("Search Found\n");
      else
        printf("Search Not Found\n");
- }
+}
 
 void
 print()
- {
+{
     int i;
     for(i = 0; i < size; i++)
 	printf("arr[%d] = %d\n",i,arr[i]);
- }
+}
 
 int
 main()
- {
+{
     init();
     insert(8);
     insert(4);
@@ -91,5 +91,4 @@ main()
     search(10);
 
     return 0;
-
- }
+}

d8e2c4a84ca0d40a86dc60c3947a2ee88cf749ae
kishori543 <kishoripnikam543@gmail.com>
2020-07-18 12:38:59 +0530

    society read and write and update data

---
+++ Team_02/society_data/society.c
@@ -0,0 +1,261 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "soc.h"
+#define size 9
+/*struct society
+{
+    int index;
+    char owner_name[30];
+    int flat_num;
+    int owner_contact;
+    struct society *temp;
+    struct society *next;
+    struct society *prev;
+};*/
+
+// struct society {
+//     struct socityData ondisk;
+//     struct society *next;
+//     struct society *prev;
+
+// }
+struct socData readsoc;
+extern void insert_soc(struct socData);
+struct society *arr[size];
+int num_records;
+
+//init array of list to NULL
+void init_soc()
+{
+    int i;
+    for(i = 1; i < size; i++) {
+        arr[i] = NULL;
+    }
+}
+
+void read_soc() {
+  int fd;
+  num_records = 0;
+  fd = open(SOCIETY_DATAFILE, O_RDWR | O_CREAT, 0644);
+  if(fd < 0) {
+     perror("read failed");
+  }
+  //printf("In the read_soc %s %d\n", __FILE__, __LINE__);
+
+  while (read(fd, (void *)&readsoc, sizeof(struct socData))) {
+  //  printf("In the read_soc %d\n", __LINE__);
+
+    //printf("%d. ", num_records);
+    printf("%d.\t%s\t\t%d\t%d\n", readsoc.index, readsoc.owner_name, readsoc.flat_num, readsoc.owner_contact);
+    insert_soc(readsoc);
+    num_records++;
+  }
+  //printf("In the read_soc %d\n", __LINE__);
+
+  close(fd);
+}
+
+//Write the student data
+void write_soc(struct socData soc)
+{
+  int fd;
+  fd = open(SOCIETY_DATAFILE, O_RDWR | O_CREAT | O_APPEND, 0644);
+  if (fd < 0) {
+    perror("file open failed...");
+  }
+  write(fd, (void *)&soc, sizeof(struct socData));
+  close(fd);
+}
+
+
+//update to file
+void update_stud_file(struct socData update)
+{
+  int fd;
+  fd = open(SOCIETY_DATAFILE, O_RDWR, 0644);
+  lseek (fd, update.index * sizeof (struct socData), SEEK_SET);
+  if (write(fd, &update, sizeof(struct socData)) < 0) {
+      perror("write failed");
+      exit(1);
+  }
+  close(fd);
+}
+
+/*society insertion*/
+void insert_soc(struct socData soc_data)
+{
+      //create a newnode with value
+      struct society *newNode = (struct society*)malloc(sizeof(struct society));
+      newNode->sd.index = soc_data.index;
+      strcpy(newNode->sd.owner_name, soc_data.owner_name);
+      newNode->sd.flat_num = soc_data.flat_num;
+      newNode->sd.owner_contact = soc_data.owner_contact;
+      newNode->next = NULL;
+      newNode->prev = NULL;
+      //calculate hash key
+      int key = soc_data.flat_num % size;
+
+      if (arr[key] == NULL) {
+          newNode->next = NULL;
+          newNode->prev = NULL;
+          arr[key] = newNode;
+      } else {
+          struct society *temp = arr[key];
+
+          while (temp->next != NULL) {
+              temp = temp->next;
+          }
+          temp->next = newNode;
+          newNode->prev = temp;
+        }
+        //printf("Node inserted\n");
+}
+
+/*void delete_soc(int flat_num)
+{
+    int key = flat_num % size;
+    struct society *ptr = arr[key], *toDelete;
+
+    if (ptr == NULL) {
+        printf("\n\n\tList is Empty !!!\n");
+    }
+    else if (ptr->flat_num == flat_num) {
+        arr[key] = arr[key]->next;
+        arr[key]->prev = NULL;//segmentation fault
+        ptr->next = NULL;
+        free(ptr);
+	printf("\n\n\tFirst node deleted\n");
+    } else {
+	while (ptr->next != NULL) {
+            if (ptr->next->flat_num == flat_num) {
+                toDelete = ptr->next;
+                if (toDelete->next == NULL) {
+                    ptr->next = NULL;
+                    free(toDelete);
+		    printf("\n\n\tLast Node is deleted\n");
+                    return;
+                } else {
+                    ptr->next = toDelete->next;
+                    toDelete->next->prev = toDelete->prev;
+                    printf("\n\n\tnode is deleted\n");
+                    free(toDelete);
+                }
+            }
+            ptr = ptr->next;
+        }
+    }
+}*/
+
+/*display data*/
+void display_soc()
+{
+  int i;
+  //int index = 1;
+  struct society *temp;
+  printf("\n==================================================================\n\n");
+  printf("SR. OW_NAME    FLAT_NO  OW_CONTACT\n\n");
+  for(i = 0; i < size; i++) {
+    temp = arr[i];
+    while(temp) {
+        //printf("%d. ", index);
+        printf("%d\t %s\t %d\t%d\n", temp->sd.index, temp->sd.owner_name, temp->sd.flat_num, temp->sd.owner_contact);
+        temp = temp->next;
+        //index++;
+    }
+  }
+}
+
+/*serch data*/
+void search_soc(int flat_num)
+{
+    struct society *ptr;
+    int i=0, flag;
+
+    int key = flat_num % size;
+
+    ptr = arr[key];
+    if (ptr == NULL) {
+        printf("\n\n\tEmpty List\n");
+    } else {
+
+
+        while (ptr != NULL) {
+            if (ptr->sd.flat_num == flat_num) {
+                printf("\n\n\tSociety flat number found at location : %d ", key-1 );
+                printf("\n\n\towner_name\t -\t %s\n\tflat_num\t -\t %d\n\towner_contact\t -\t %d\n", ptr->sd.owner_name, ptr->sd.flat_num, ptr->sd.owner_contact);
+                flag = 0;
+                break;
+            } else {
+                flag=1;
+            }
+            i++;
+            ptr = ptr -> next;
+        }
+        if (flag==1) {
+            printf("\n\n\tsociety flat number not found\n");
+        }
+    }
+}
+
+
+/*updation*/
+void update_soc(int flat_num)
+{
+    struct society *ptr;
+    int i=0, flag;
+
+    int key = flat_num % size;
+
+    ptr = arr[key];
+    if (ptr == NULL) {
+        printf("\n\n\tEmpty List\n");
+    } else {
+
+        while (ptr != NULL) {
+            if (ptr->sd.flat_num == flat_num) {
+                printf("\n\n\tSociety old Data !!!\n");
+                printf("\n\n\tsociety Index - %d\n\tsociety owner_name - %s\n\tsociety flat_num - %d\n\tsociety owner_contact - %d",
+                       ptr->sd.index, ptr->sd.owner_name, ptr->sd.flat_num, ptr->sd.owner_contact);
+
+            		printf("\n\n\tSociety New Data !!!\n");
+
+            		int flat_num, owner_contact, index;
+                char owner_name[30];
+
+                printf("\n\tEnter Same Index : ");
+                scanf("\t %d", &index);
+            		printf("\n\tEnter New owner Name : ");
+            		scanf("\t %[^\n]%*c", owner_name);
+                printf("\n\tEnter Same flat number : ");
+            		scanf("\t %d", &flat_num);
+                printf("\n\tEnter New Contact : ");
+            		scanf("\t %d", &owner_contact);
+
+                ptr->sd.index = index;
+                ptr->sd.flat_num = flat_num;
+            		strcpy(ptr->sd.owner_name, owner_name);
+          	    ptr->sd.owner_contact = owner_contact;
+
+          	    printf("\n\n\tsociety Index - %d\n\tsociety owner_name- %s\n\tsociety flat_num- %d\n\tsociety owner_contact - %d", ptr->sd.index, ptr->sd.owner_name, ptr->sd.flat_num, ptr->sd.owner_contact);
+          	    printf("\n\n\tSociety Record Updated Successfully !!!\n");
+                flag = 0;
+                update_stud_file(ptr->sd);
+            break;
+          } else {
+                    flag = 1;
+              }
+            i++;
+            ptr = ptr -> next;
+        }
+
+        if (flag == 1) {
+            printf("\n\n\tsociety flat_num not found\n");
+        }
+    }
+}

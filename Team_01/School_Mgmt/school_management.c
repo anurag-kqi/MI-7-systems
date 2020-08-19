29e75fdad80e4a92c8a1d8ea9da2c450a2a1f060
Anurag Agarwal <anurag.agarwal@cookie.ai>
2020-08-19 09:35:31 +0530

    multi client

--- Team_01/School_Mgmt/school_management.c
+++ Team_01/School_Mgmt/school_management.c
@@ -1,6 +1,5 @@
 /*School Mnagement Systems*/
 #include "header.h"
-#define num_client 5;
 
 void *conn_handlar(void *sock_desc);
 
@@ -33,91 +32,114 @@ extern int num_records;
 
 void error(const char *msg) {
 	perror(msg);
-	exit(1);
+    printf("Exiting thread %ld\n", pthread_self());
+    pthread_exit(NULL);
 }
-
 
 int
 main(int argc, char *argv[])
 {
+    int num_client = 5;
+	int sock_desc, i;
+    pthread_t thread;
+    int sockfd, newsockfd, portno;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
 
-		int sock_desc, i;
-		pthread_t thread;
+    if (argc < 2) {
+            fprintf(stderr,"ERROR, no port provided\n");
+            exit(1);
+    }
 
-		for (i = 0; i < num_client; i++) {
-				if (pthread_create(&thread, NULL, conn_handlar, (void*)i) < 0) {
-						perror("could not create thread");
-						return 1;
-				}
-				sleep(3);
-		}
-		pthread_exit(NULL);
+    init_stud();
+	init_teacher();
+
+	//reading student datafile
+	printf("\n_______________________________________________________________________________\n");
+	printf("\t\tSTUDENT DATAFILE\n\n");
+	read_stud();
+
+	//reading teacher datafile
+	printf("\n_______________________________________________________________________________\n");
+	printf("\t\tTEACHER DATAFILE\n\n");
+	read_teach();
+
+    portno = atoi(argv[1]);
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		 error("ERROR opening socket");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+					 sizeof(serv_addr)) < 0)
+					 error("ERROR on binding");
+	listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    printf("Server thread  %ld waiting for accept\n", pthread_self());
+
+    while (1) {
+    	newsockfd = accept(sockfd,
+    							(struct sockaddr *) &cli_addr,
+    							&clilen);
+    	if (newsockfd < 0)
+    			 error("ERROR on accept");
+
+        if (pthread_create(&thread, NULL, conn_handlar,
+                           (void *)&newsockfd) < 0) {
+         				perror("could not create thread");
+         				return 1;
+        }
+        printf("Main: New client thread %ld newsockfd %d\n", thread, newsockfd);
+    }
+
+
+	// for (i = 0; i < num_client; i++) {
+    //     printf("creating thread %d \n", i);
+	// 	if (pthread_create(&thread, NULL, conn_handlar, (void *)portno) < 0) {
+	// 			perror("could not create thread");
+	// 			return 1;
+	// 	}
+	// 	sleep(3);
+	// }
+
+    printf("Exiting main\n");
+	pthread_exit(NULL);
+    close(sockfd);
 
     return 0;
 }
 
-void
-*conn_handlar(void *sock_desc)
+void *
+conn_handlar(void *arg)
 {
-		int sockfd, newsockfd, portno;
-		socklen_t clilen;
-		char buffer[1024];
-		struct sockaddr_in serv_addr, cli_addr;
-		int n;
-		if (argc < 2) {
-				fprintf(stderr,"ERROR, no port provided\n");
-				exit(1);
-		}
-		sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		if (sockfd < 0)
-			 error("ERROR opening socket");
-		bzero((char *) &serv_addr, sizeof(serv_addr));
-		portno = atoi(argv[1]);
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_addr.s_addr = INADDR_ANY;
-		serv_addr.sin_port = htons(portno);
-		if (bind(sockfd, (struct sockaddr *) &serv_addr,
-						 sizeof(serv_addr)) < 0)
-						 error("ERROR on binding");
-		listen(sockfd,5);
-		clilen = sizeof(cli_addr);
-		newsockfd = accept(sockfd,
-								(struct sockaddr *) &cli_addr,
-								&clilen);
-		if (newsockfd < 0)
-				 error("ERROR on accept");
-		//init array of list to NULL
-		init_stud();
-		init_teacher();
+	int newsockfd;
+	char buffer[1024];
+	int n;
+    int ch, id, index,contact,j,digit,alpha;
+    char name[30], address[50], class[10], department[30];
 
-		//reading student datafile
-		printf("\n_______________________________________________________________________________\n");
-		printf("\t\tSTUDENT DATAFILE\n\n");
-		read_stud();
-
-		//reading teacher datafile
-		printf("\n_______________________________________________________________________________\n");
-		printf("\t\tTEACHER DATAFILE\n\n");
-		read_teach();
-
-		int ch, id, index,contact,j,digit,alpha;
-		char name[30], address[50], class[10], department[30];
+    newsockfd = *(int *)arg;
+    printf("In thread %ld newsockfd %d\n", pthread_self(), newsockfd);
+	//init array of list to NULL
 
 	while (1) {
-				read(newsockfd, &ch, sizeof(int));
-				switch (ch) {
-						case 1:
-									read(newsockfd, &ch, sizeof(int));
-									switch (ch) {
-											case 1:
+		read(newsockfd, &ch, sizeof(int));
+		switch (ch) {
+			case 1:
+								read(newsockfd, &ch, sizeof(int));
+								switch (ch) {
+								case 1:
 								write(newsockfd, &num_records, sizeof(int));
 								read(newsockfd, &stud, sizeof(struct student_disk));
 								insert_stud(stud);
 								write_stud(stud);
 								num_records++;
-															break;
+								break;
 
-											case 2:
+								case 2:
 								write(newsockfd, &num_record, sizeof(int));
 								read(newsockfd, &teach, sizeof(struct teacher_disk));
 								insert_teach(teach);
@@ -215,6 +237,5 @@ void
 
 
 		close(newsockfd);
-		close(sockfd);
 
 }
--- know_base/clang/concepts.md
+++ know_base/clang/concepts.md
@@ -5,8 +5,8 @@ Ex:    ./client localhost
     So if argc and argv are used in main. Then argc value will be 2 and argv[0] will be "client" and argv[1] will be "localhost".
 
 
-Q2: 
+Q2: When to use enum?
+- enum should be used to avoid magic numbers in the code
 
 
 Q3:
-

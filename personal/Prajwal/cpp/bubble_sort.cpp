0a83c25795a7edca1cd43922ff04f0ca9cf72efc
PrajwalYadav <yadavprajwal1897@gmail.com>
2020-08-24 15:35:11 +0530

    Done makefile and binary search

--- personal/Prajwal/cpp/bubble_sort.cpp
+++ personal/Prajwal/cpp/bubble_sort.cpp
@@ -14,10 +14,10 @@ void bubbleSort(int arr[], int n)
             }
         }
     }
-    cout << "Sorted array bubble sort: \n";
+    cout << "\nSorted array bubble sort: \n";
     for (i = 0; i < n; i++)
 		cout << arr[i] << " ";
-	cout << endl;
+	cout << endl << endl;
 }
 
 int partition (int arr[], int low, int high)
@@ -49,25 +49,70 @@ void quickSort(int arr[], int low, int high)
 
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
-    }    
+    }
 }
 
-void printArray(int arr[], int size)
+int binarySearch(int arr[], int l, int r, int key)
 {
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[mid] < key)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return false;
 }
 
 int main()
 {
-	int arr[] = {64, 34, 25, 12, 22, 11, 90};
-	int n = sizeof(arr)/sizeof(arr[0]);
-    cout << "size : " << n << "\n";
-	bubbleSort(arr, n);
-    quickSort(arr, 0, n - 1);
-    cout << "Sorted array QuickSort: \n";
-    printArray(arr, n);
+    int arr[50], n, ch, key, result;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    while(1) {
+        cout << "\nMENU\n1. Bubble Sort\n2. Quick Sort\n3. Binary Search\n4. Exit\n\nEnter your choice: ";
+        cin >> ch;
+        switch(ch)
+        {
+            case 1 :
+                    bubbleSort(arr, n);
+                    break;
+
+            case 2 :
+                    quickSort(arr, 0, n - 1);
+                    cout << "\nSorted array QuickSort: \n";
+                    for (int i = 0; i < n; i++)
+                        cout << arr[i] << " ";
+                    cout << endl << endl;
+                    break;
+
+            case 3 :
+                    cout << "Enter element to search : ";
+                    cin >> key;
+                    result = binarySearch(arr, 0, n-1, key);
+                    if (result == false) {
+                        cout<<"\nElement not found....\n";
+                    } else {
+                        cout<<"\nElement is found....\n";
+                    }
+                    break;
+
+            case 4 :
+                    exit(0);
+
+            default:
+                    cout << "Wrong choice..\n";
+                    break;
+        }
+    }
 	return 0;
 }

2ba7271c0353e8429e36ecda3c668f53113fb68a
Divyani286 <divyanipatidar307@gmail.com>
2020-08-27 16:57:30 +0530

    string program

---
+++ personal/Divyani/vcode/string.cpp
@@ -0,0 +1,19 @@
+//C++ program to display a string entered by user.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[100];
+
+    cout << "Enter a string:- ";
+    cin >> str;
+    cout << "You entered:- " << str << endl;
+
+    cout << "\nEnter another string:- ";
+    cin >> str;
+    cout << "You entered:- "<<str<<endl;
+
+    return 0;
+}
